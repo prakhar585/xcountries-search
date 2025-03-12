ad22a2309ab8d607b6828b2bbf9e3a681ab09c35
unknown <prakharchaturvedi9860@gmail.com>
2025-03-12 19:34:24 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
+  const [maps, setMaps] = useState([]);
+
+
+  //fetch the data
+  useEffect(()=>{
+    const getData= async () =>{
+      try{
+        const response = await axios.get(`https://xcountries-backend.azurewebsites.net/all`);
+        setMaps(response.data);
+        
+      }catch(error){
+        console.error('Error fetching data:', error);
+      }
+    }
+
+
+    getData();
+  },[])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      </>
+    
   );
 }
 
